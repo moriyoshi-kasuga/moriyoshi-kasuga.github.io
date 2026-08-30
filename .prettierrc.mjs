819487fcf3b5a3bc31d7263b2c0cd56946e92f13
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-08-30 11:34:39 +0900

    chore: add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+export default {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,10 @@ import antfu from "@antfu/eslint-config";
 
 export default antfu(
   {
-    stylistic: {
-      indent: 2,
-      semi: true,
-      quotes: "double",
-    },
+    stylistic: false,
     rules: {
       "import/no-self-import": "off",
       "@typescript-eslint/no-namespace": "off",
-      "style/arrow-parens": "off",
-      "style/multiline-ternary": "off",
     },
     astro: true,
   },
